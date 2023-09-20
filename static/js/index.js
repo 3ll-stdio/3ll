fdd757c3dcd0bb193d989068c5c46906a534c09e
Fotios Pechlivanis <fotispechli@gmail.com>
2023-09-20 18:40:50 +0300

    Remove scroll event

--- static/js/index.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener(
-  "wheel",
-  (e) => {
-    if (window.innerWidth >= 992)
-    e.preventDefault();
-    document.querySelector(".app").scrollLeft += e.deltaY * 4;
-  },
-  { passive: false }
-);
